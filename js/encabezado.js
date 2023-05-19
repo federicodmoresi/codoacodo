096a9fafe2887b3c5f3a0d19a4cf1da83e993cb1
Federico Moresi <100998017+federicodmoresi@users.noreply.github.com>
2023-05-19 13:03:41 +0200

    nueva api del clima

--- js/encabezado.js
+++
@@ -1,7 +0,0 @@
-let encabezado=document.querySelector('#encabezado')
-let paginaActual=document.querySelector
-encabezado.innerHTML = `
-<a href="index.html">HOME</a>
-<a href="nosotros.html">NOSOTROS</a>
-<a href="productos.html">PRODUCTOS</a>
-<a class="pagina-actual" href="contacto.html">CONTACTO</a>`
